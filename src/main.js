f57c46926f3b7a9fbe986b4ac12de57807e7c75b
issac-cosmos <rud____@naver.com>
2025-02-11 17:00:25 +0900

    first

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// 중괄호가 쳐져있으면 , 메서드를 import. 중괄호가 없으면 해당파을자체를 import
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
+import vuetify from './plugins/vuetify';
+
+const app = createApp(App);
+
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
