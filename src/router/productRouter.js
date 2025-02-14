f2aecfe8411118fd208dc0768ac18af75cddb760
issac-cosmos <rud____@naver.com>
2025-02-14 09:02:04 +0900

    발렌타인데이

---
+++ src/router/productRouter.js
@@ -0,0 +1,12 @@
+import ProductList from "@/views/productList.vue";
+
+
+export const productRouter = [
+    {
+        path: '/product/list',
+        name: 'productList',
+        component: ProductList
+    },
+    
+
+]
